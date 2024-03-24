b3ef163811cfc184cceb006c4e2c7e3ab2670aac
fmpberger88 <fabian.berger@protonmail.ch>
2024-03-24 18:06:53 +0100

    refactor player function and addig getScore and updateScore

--- utils/player.js
+++ utils/player.js
@@ -1,3 +1,10 @@
-const player = (name) => ({ name });
+function player(name) {
+  let score = 0;
+  const active = false;
+  const getScore = () => score;
+  const updateScore = () => score += 1;
+
+  return { name, getScore, updateScore };
+}
 
 export { player };
